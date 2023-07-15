e83b7c80f2c36b5a82140ebe480a14303b4cd152
Lukas Friesecke <lukas.friesecke@st.ovgu.de>
2023-07-15 02:07:29 +0200

    feat: Read file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    // read command line arguments:
+    let args: Vec<String> = env::args().collect();
+    let file_path = &args[1];
+
+    // read and print content of specified file:
+    let file_content = fs::read_to_string(file_path)
+        .expect("Something went wrong.");
+
+    println!("File content:\n{file_content}")
 }

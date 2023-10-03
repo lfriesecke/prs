a97267df4591ad42be67f80fb822f17179ed81c6
Lukas Friesecke <lukas.friesecke@st.ovgu.de>
2023-10-03 11:59:02 +0200

    refactor: parsing arguments

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs;
+use std::process;
 use std::vec;
 
 use content_elements::ContentElement;
@@ -11,10 +12,13 @@ fn main() {
     
     // read command line arguments:
     let args: Vec<String> = env::args().collect();
-    let file_path = &args[1];
+    let config = markdown_parser::Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
     // read and print content of specified file:
-    let file_content = fs::read_to_string(file_path)
+    let file_content = fs::read_to_string(config.file_path)
         .expect("Error while reading the specified file.");
 
     for val in file_content.lines() {
--- src/markdown_parser.rs
+++ src/markdown_parser.rs
@@ -1,5 +1,15 @@
-pub fn test() {
-    println!("Test");
+pub struct Config {
+    pub file_path: String,
+}
+
+impl Config { 
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 2 {
+            return Err("not enough arguments");
+        }
+
+        Ok(Config { file_path: args[1].clone() } )
+    }
 }
 
 pub fn split_slides(file_content: String) -> Vec<String> {
